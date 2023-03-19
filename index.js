09837eacb39f1e1cd46a38d8fa5cedfeaa8da329
Joseph Schmitt <codewaggle@gmail.com>
2023-03-19 15:58:20 -0400

    phase1 artitmetic

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+
+
+
+let num1 = 1;
+let num2 = 62;
+
+const multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100);
+
+const num3 = 32;
+const num4 = 7;
+
+let mod = num3 % num4;
+
+let numSet = (1, 2, 5, 7, 20);
+
+let max = Math.max(1, 2, 5, 7, 20);
